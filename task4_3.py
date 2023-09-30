35f19fa7938231fcd4553a78f274422314a8578f
evgeniasotnikova <sotnikova.zhenya@mal.ru>
2023-09-30 21:35:24 +0300

    task4.py

--- task4_3.py
+++ task4_3.py
@@ -1,10 +1,23 @@
 def is_anagram(word1, word2):
     return sorted(word1) == sorted(word2)
 
-word1 = input("Введите первое предложение: ")
-word2 = input("Введите второе предложение: ")
 
-if is_anagram(word1, word2):
+word1 = input("Введите первое предложение: ").lower()
+word2 = input("Введите второе предложение: ").lower()
+
+word3 = ''
+word4 = ''
+
+alp = 'abcdefghijklmopqrstuvwxyz'
+for i in word1:
+    if i in alp:
+        word3+=i
+
+for i in word2:
+    if i in alp:
+        word4 += i
+
+if is_anagram(word3, word4):
     print(True)
 else:
     print(False)
