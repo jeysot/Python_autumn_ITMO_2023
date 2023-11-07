8d3e7e07eba02e86cb9d00f3de07210b9b075851
evgeniasotnikova <sotnikova.zhenya@mal.ru>
2023-11-07 16:11:32 +0300

    task20.py

--- Task 20/Task20_3.py
+++ Task 20/Task20_3.py
@@ -1,29 +1,5 @@
-# class Task:
-#     def __init__(self):
-#         self.index = 1
-#         self.letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-#
-#     def __iter__(self):
-#         return self
-#
-#     def __next__(self):
-#         if self.index > 26:
-#             self.index = 1
-#         current_index = self.index
-#         self.index += 1
-#         if current_index % 26 == 0:
-#             letter = self.letters[-1]
-#         else:
-#             letter = self.letters[current_index % 26 - 1]
-#         return current_index, letter
-#
-#
-# s = Task()
-#
-#
-# for _ in range(30):
-#     print(*next(s), end=',')
-class CyclicSequenceGenerator:
+
+class Task:
     def __init__(self):
         self.current_number = 1
         self.current_letter = 'A'
@@ -34,7 +10,6 @@ class CyclicSequenceGenerator:
     def __next__(self):
         element = f"{self.current_number}, {self.current_letter}"
 
-        # Обновляем текущее число и букву
         if self.current_number == 26:
             self.current_number = 1
             if self.current_letter == 'Z':
@@ -47,8 +22,7 @@ class CyclicSequenceGenerator:
 
         return element
 
-# Пример использования:
-generator = CyclicSequenceGenerator()
+s = Task()
 for _ in range(50):
-    print(next(generator), end=', ')
+    print(next(s), end=', ')
 
